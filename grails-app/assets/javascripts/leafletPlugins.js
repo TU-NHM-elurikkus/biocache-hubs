6a0b0f4f10d238eef5af417ab4c497cf6b0dac4b
timo-piirmann <timurlane5@gmail.com>
2018-09-27 14:43:33 +0300

    Remove invalid asset references + magellan.js

--- grails-app/assets/javascripts/leafletPlugins.js
+++ grails-app/assets/javascripts/leafletPlugins.js
@@ -1,6 +1,5 @@
 //= require leaflet-1.2.0/leaflet-src
 //= require leaflet-plugins/fullscreen/Control.FullScreen
-//= require leaflet-plugins/layer/tile/Google
 //= require leaflet-plugins/spin/spin.min
 //= require leaflet-plugins/spin/leaflet.spin
 //= require leaflet-plugins/coordinates/Leaflet.Coordinates-0.1.4.min
